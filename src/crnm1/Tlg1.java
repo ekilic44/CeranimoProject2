b9794ad5f6bc3059915cb8d092d23265994aa1b8
Eyup K <ekilic3844@gmail.com>
2023-07-04 13:17:46 +0300

    Added sth

--- src/crnm1/Tlg1.java
+++ src/crnm1/Tlg1.java
@@ -4,5 +4,6 @@ public class Tlg1 {
     public static void main(String[] args) {
         System.out.println("work1 started.");
         System.out.println("sth added");
+        System.out.println("LiCbr");
     }
 }
